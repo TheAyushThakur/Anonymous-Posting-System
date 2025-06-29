af1438367f5632707727ea9f7c3c096c51ce2754
TheAyushThakur <ayushthakur6005@gmail.com>
2025-06-29 11:26:58 +0530

    Added a path in urls.py for post_detail

--- forum/urls.py
+++ forum/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name= 'home'),
     path('like/<int:post_id>/', views.like_post, name='like_post'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+
 ]
