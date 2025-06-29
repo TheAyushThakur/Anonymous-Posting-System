eeec7a19e42f11b7b3907caf4c11b76497b49ba1
TheAyushThakur <ayushthakur6005@gmail.com>
2025-06-29 11:25:31 +0530

    Added Post_detail function in views.py for a proper detailed section for a post

--- forum/views.py
+++ forum/views.py
@@ -44,3 +44,24 @@ def like_post(request, post_id):
     post.likes += 1
     post.save()
     return HttpResponseRedirect(reverse('home'))
+
+def post_detail(request, post_id):
+    post = get_object_or_404(AnonymousPost, pk=post_id)
+    comments = post.comments.order_by('-created_at')
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.created_at = timezone.now()
+            comment.save()
+            return redirect('post_detail', post_id=post_id)
+    else:
+        form = CommentForm()
+
+    return render(request, 'post_detail.html', {
+        'post': post,
+        'form': form,
+        'comments': comments
+    })
